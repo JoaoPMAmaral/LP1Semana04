d44be5fd9f9489a113eed26b5dee6182d24fd4cb
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-19 15:04:28 +0000

    create betterdecorador and update solution

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string texto = args[0];
+            string simbolo = args[1];
+            string vezesSimbolo = args[2];
+
+            int vezesSimboloInt = int.Parse(vezesSimbolo);
+
+            for(int i = 0; i < vezesSimboloInt; i++)
+            {
+                Console.Write(simbolo);
+            }
+            
+            Console.Write(" ");
+            Console.Write(texto);
+            Console.Write(" ");
+
+            for(int i = 0; i < vezesSimboloInt; i++)
+            {
+                Console.Write(simbolo);
+            }
+        }
+    }
+}
