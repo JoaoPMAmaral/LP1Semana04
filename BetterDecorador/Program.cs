cd072f4d7c30e50a896d833f294213ad31c50bf0
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-19 15:11:59 +0000

    update betterdecorador to be a function and not printed directly

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,27 +4,36 @@ namespace BetterDecorador
 {
     public class Program
     {
+        private static string Decor(string s, char c, int v)
+        {
+            string final = "";
+
+            for(int i = 0; i < v; i++)
+            {
+                final += c;
+            }
+
+            final += " ";
+            final += s;
+            final += " ";
+
+            for(int i = 0; i < v; i++)
+            {
+                final += c;
+            }
+
+            return final;
+        }
+
         private static void Main(string[] args)
         {
-            string texto = args[0];
             string simbolo = args[1];
             string vezesSimbolo = args[2];
 
+            char simboloChar = char.Parse(simbolo);
             int vezesSimboloInt = int.Parse(vezesSimbolo);
 
-            for(int i = 0; i < vezesSimboloInt; i++)
-            {
-                Console.Write(simbolo);
-            }
-            
-            Console.Write(" ");
-            Console.Write(texto);
-            Console.Write(" ");
-
-            for(int i = 0; i < vezesSimboloInt; i++)
-            {
-                Console.Write(simbolo);
-            }
+            Console.WriteLine(Decor(args[0],simboloChar,vezesSimboloInt));
         }
     }
 }
