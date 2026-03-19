b1401a5c05dc5c23d58042360a578aad687e846a
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-03-19 15:31:06 +0000

    added a failsafe for no args

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,9 +4,21 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// grabs 3 arguments from user and returns a string
+        /// </summary>
+        /// <param name="s">string in between decorations</param>
+        /// <param name="c">character for decorating before and after</param>
+        /// <param name="v">number of times character is repeated</param>
+        /// <returns>a final string from the addition of everything</returns>
         private static string Decor(string s, char c, int v)
         {
             string final = "";
+         
+            if(s.Length == 0)
+            {
+                final += "User did not specify args";
+            }
 
             for(int i = 0; i < v; i++)
             {
@@ -25,15 +37,26 @@ namespace BetterDecorador
             return final;
         }
 
+        /// <summary>
+        /// calls decor and user's parameters
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
-            string simbolo = args[1];
-            string vezesSimbolo = args[2];
+            if(args.Length != 0)
+            {
+                string simbolo = args[1];
+                string vezesSimbolo = args[2];
 
-            char simboloChar = char.Parse(simbolo);
-            int vezesSimboloInt = int.Parse(vezesSimbolo);
+                char simboloChar = char.Parse(simbolo);
+                int vezesSimboloInt = int.Parse(vezesSimbolo);
 
-            Console.WriteLine(Decor(args[0],simboloChar,vezesSimboloInt));
+                Console.WriteLine(Decor(args[0],simboloChar,vezesSimboloInt));
+            }
+            else
+            {
+                Console.WriteLine(Decor("User did not specify args!",'=',3));
+            }
         }
     }
 }
